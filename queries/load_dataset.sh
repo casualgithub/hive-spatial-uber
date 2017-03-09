a8f7db746ce2f7b5424f0c74b123cf0fdf7df6f9
Carter Shanklin <carter@HW11972.local>
2017-03-09 14:44:07 -0800

    Better queries

--- queries/load_dataset.sh
+++ queries/load_dataset.sh
@@ -11,15 +11,17 @@ cat<<EOF>load.sql
 drop table if exists uber;
 drop table if exists uber_orc;
 create table uber (
-	id int,
-	ts string,
-	long double,
-	lat double
+ id int,
+ ts string,
+ long double,
+ lat double
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
 STORED AS TEXTFILE;
 load data local inpath 'all.tsv' overwrite into table uber;
 create table uber_orc stored as orc as select * from uber;
+analyze table uber_orc compute statistics;
+analyze table uber_orc compute statistics for columns;
 EOF
 
 hive -f load.sql
@@ -30,5 +32,26 @@ from
 ( select id, long, lat, row_number() over ( partition by id order by ts desc ) as rn from uber_orc ) sub1,
 ( select id, long, lat, row_number() over ( partition by id order by ts desc ) as rn from uber_orc ) sub2
 where sub1.rn = 1 and sub2.rn = 1 and
-sqrt( pow(sub1.long - sub2.long, 2) + pow(sub1.lat - sub2.lat, 2) ) <= (1/(111 * 10));
+sqrt( pow(sub1.long - sub2.long, 2) + pow(sub1.lat - sub2.lat, 2) ) <= (1/(111 * 25));
 EOF
+
+cat<<EOF>hotspotend.sql
+select id, long, lat, count(*) c from (
+select sub1.id as id, sub2.id as id2, sub1.long, sub1.lat, sub2.long long2, sub2.lat lat2, sqrt( pow(sub1.long - sub2.long, 2) + pow(sub1.lat - sub2.lat, 2) ) * 111 as distance_km
+from
+( select id, long, lat, row_number() over ( partition by id order by ts desc ) as rn from uber_orc ) sub1,
+( select id, long, lat, row_number() over ( partition by id order by ts desc ) as rn from uber_orc ) sub2
+where sub1.rn = 1 and sub2.rn = 1 and
+sqrt( pow(sub1.long - sub2.long, 2) + pow(sub1.lat - sub2.lat, 2) ) <= (1/(111 * 10)) ) sub group by id, long, lat order by c;
+EOF
+
+cat<<EOF>hotspotstart.sql
+select id, long, lat, count(*) c from (
+select sub1.id as id, sub2.id as id2, sub1.long, sub1.lat, sub2.long long2, sub2.lat lat2, sqrt( pow(sub1.long - sub2.long, 2) + pow(sub1.lat - sub2.lat, 2) ) * 111 as distance_km
+from
+( select id, long, lat, row_number() over ( partition by id order by ts ) as rn from uber_orc ) sub1,
+( select id, long, lat, row_number() over ( partition by id order by ts ) as rn from uber_orc ) sub2
+where sub1.rn = 1 and sub2.rn = 1 and
+sqrt( pow(sub1.long - sub2.long, 2) + pow(sub1.lat - sub2.lat, 2) ) <= (1/(111 * 10)) ) sub group by id, long, lat order by c;
+EOF
+
